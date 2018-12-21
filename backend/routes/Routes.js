e14cca6092463d96e89578bcc13a4de96ba0616f
yuanda26 <yuanda.dian@gmail.com>
2018-12-21 16:15:21 +0700

    Add Master Souvenir API

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -1,6 +1,15 @@
 const unitLogic = require("../bisnislogics/M_Unit_Logic");
+const souvenirLogic = require("../bisnislogics/M_Souvenir_Logic");
 
 module.exports = server => {
+  // Master Souvenir Route
+  server.get("/api/souvenir", souvenirLogic.readAllSouvenir);
+  server.get("/api/souvenir/:souvenirId", souvenirLogic.readSouvenirById);
+  server.post("/api/souvenir", souvenirLogic.createSouvenir);
+  server.put("/api/souvenir/:souvenirId", souvenirLogic.updateSouvenir);
+  server.del("/api/souvenir/:code", souvenirLogic.deleteSouvenir);
+  //== End of Master Souvenir Route
+
   // Master Unit Route
   server.get("/api/unit", unitLogic.readAllUnit);
   server.get("/api/unit/:unitId", unitLogic.readOneById);
