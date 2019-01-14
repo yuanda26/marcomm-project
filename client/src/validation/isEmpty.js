2f9e6bf59915ff68d3de45c0abb8bd2c59c03e5c
yuanda26 <yuanda.dian@gmail.com>
2019-01-14 15:02:59 +0700

    Add Other Conditions

--- client/src/validation/isEmpty.js
+++ client/src/validation/isEmpty.js
@@ -2,7 +2,9 @@ const isEmpty = value =>
   value === undefined ||
   value === null ||
   value === "" ||
+  value.length === 0 ||
   (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "object" && value.length === 0) ||
   (typeof value === "string" && value.trim().length === 0);
 
 export default isEmpty;
